cdb84e7ba4836c534698df6f61d1f7d483261573
Orish <hsirov321@gmail.com>
2020-06-12 10:31:11 +0545

    some ui

--- public/js/GetBills.js
+++ public/js/GetBills.js
@@ -23,15 +23,15 @@ let showHistory = () => {
 
   history.map((h, key) => {
     cards.innerHTML += `
-    <div class="card-inner">
-    <div class="card-title space-evenly"> 
-    <div> <i class="material-icons">date_range</i><strong>${h.date}</strong></div><br/>
-    <div> <i class="material-icons">person</i><strong></strong>${h.name}</div> 
-    </div>
-    <div class="card-body space-between">
-    <div>Rs. <strong>${h.sumtotal}</strong></div>
-    <i class="large material-icons" onclick ="showTabHistory(${h.id})">arrow_forward</i>
-    </div>
+    <div onclick ="showTabHistory(${h.id})" class="card-inner">
+      <div class="card-title space-evenly"> 
+        <div> <i class="material-icons">date_range</i><strong>${h.date}</strong></div><br/>
+        <div> <i class="material-icons">person</i><strong></strong>${h.name}</div> 
+      </div>
+      <div class="card-body space-between">
+      <div>Rs. <strong>${h.sumtotal}</strong></div>
+      <i class="large material-icons" onclick ="showTabHistory(${h.id})">arrow_forward</i>
+      </div>
     </div>`;
   });
 };
